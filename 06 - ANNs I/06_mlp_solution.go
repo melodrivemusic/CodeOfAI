f12e7d0956e91b6f386f08a504e89a652238a1a0
VasanthakumarV <vasanth260m12@gmail.com>
2019-03-25 18:54:15 +0530

    adding comments

--- 06 - ANNs I/06_mlp_solution.go
+++ 06 - ANNs I/06_mlp_solution.go
@@ -3,22 +3,28 @@ package main
 import (
 	"fmt"
 
-	"06_mlp_solution/mlp"
+	"06_mlp_solution/mlp" //mlp package to abstract away the calculations
 
-	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/mat" //gonum library used for matrix calculations
 )
 
 func main() {
+
+	// creating an mlp by defining the number of inputs, outputs
+	// and a list of ints for the hidden layers
 	m := &mlp.Mlp{
 		NumInputs:    2,
 		HiddenLayers: []int{2, 3, 4},
 		NumOutputs:   5,
 	}
 
+	// initializing the mlp by setting the random weights
 	m.Init()
 
+	// defining the input matrix based on the size provided earlier
 	input := mat.NewDense(1, m.NumInputs, []float64{1.0, 2.0})
 
+	// activate the network
 	output := m.Activate(input)
 
 	fmt.Printf("Network Activation: %v\n", mat.Formatted(output, mat.Prefix("")))
