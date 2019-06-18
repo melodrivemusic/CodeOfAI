baba1dbdd7d098b9c3198e57673fc47ef0f28c89
Andy Elmsley <andy@melodrive.com>
2019-06-18 16:03:32 +0200

    Code for week 12

--- 12 - GAs III/python/12_ga_mutation.py
+++ 12 - GAs III/python/12_ga_mutation.py
@@ -3,6 +3,9 @@ from itertools import tee
 
 
 class Item:
+    """
+    The Item class represents one item in our knapsack
+    """
     value = 0
     weight = 0
 
@@ -15,27 +18,57 @@ class Item:
 
 
 def getRandomItem(min=1, max=30):
+    """
+    Returns an Item with random weight and value
+    :param min: The minimum weight/value
+    :param max: The maximum weight/value
+    :return: Item
+    """
     return Item(
-        random.randrange(min, max),
-        random.randrange(min, max)
+        randrange(min, max),
+        randrange(min, max)
     )
 
 
 def getRandomChromosome(numItems):
+    """
+    Creates a single random chromosome
+    :param numItems: The number of genes in the chromosome
+    :return: list
+    """
     return [1 if random() > 0.5 else 0
             for x in range(numItems)]
 
 
 def getRandomPopulation(popSize, numItems):
+    """
+    Creates a population of random chromosomes
+    :param popSize: Population size
+    :param numItems: Chromosome size
+    :return: list
+    """
     return [getRandomChromosome(numItems)
             for x in range(popSize)]
 
 
 def getPhenotype(chromosome, items):
+    """
+    Given a chromosome, returns a list of items in the bag
+    :param chromosome:
+    :param items:
+    :return: list
+    """
     return [v for i, v in enumerate(items) if chromosome[i] == 1]
 
 
 def fitness(chromosome, items, capacity):
+    """
+    Calculates a fitness score for a single chromosome
+    :param chromosome:
+    :param items:
+    :param capacity:
+    :return: float
+    """
     phenotype = getPhenotype(chromosome, items)
     weight = sum([i.weight for i in phenotype])
     if weight > capacity:
@@ -45,30 +78,54 @@ def fitness(chromosome, items, capacity):
 
 
 def getFitness(population, items, capacity):
+    """
+    Calculates fitness scores for a population of chromosomes
+    :param population:
+    :param items:
+    :param capacity:
+    :return: sorted list of fitnesses [ (chromosome, score), ... ]
+    """
     fitnessScores = []
     for chromosome in population:
         score = fitness(chromosome, items, capacity)
         fitnessScores.append((chromosome, score))
-    fitnessScores = sorted(fitnessScores, key=lambda x: x[1], reverse=True)
-    return fitnessScores
+    return sorted(fitnessScores, key=lambda x: x[1], reverse=True)
 
 
-def select(fitnessScores, selectionRatio=0.5):
-    numToSelect = int(len(fitnessScores)*selectionRatio)
+def select(population, *fitnessArgs, selectionRatio=0.5):
+    """
+    Selects the fittest members of the poplation based on a fitness function and ratio
+    :param population:
+    :param fitnessArgs: The arguments passed to the fitness function
+    :param selectionRatio: how many items to select/reject
+    :return: tuple (selected, rejected)
+    """
+    numToSelect = int(len(population) * selectionRatio)
+    # calculate fitness for the population
+    fitnessScores = getFitness(population, *fitnessArgs)
     selected = fitnessScores[:numToSelect]
     rejected = fitnessScores[numToSelect:]
-    return [x[0] for x in selected], [x[0] for x in rejected]
+    return selected, rejected
 
 
 def pairwise(iterable):
+    """
+    Iterates over a list, two items at a time
+    :param iterable:
+    :return: iterable
+    """
     a, b = tee(iterable)
     next(b, None)
     return zip(a, b)
 
 
 def crossover(parents):
+    """
+    Creates a random crossover of a population
+    :param parents:
+    :return: list the created children
+    """
     children = []
-    parents = parents[:]
     shuffle(parents)
     chromosomeLength = len(parents[0])
     halfLength = int(chromosomeLength*0.5)
@@ -93,27 +150,47 @@ def crossover(parents):
     return children
 
 
-def mutate(population, mutationChance=0.1):
+def mutate(chromosome, mutationChance=0.1):
+    """
+    Mutate a chromosome by flipping bits
+    :param chromosome:
+    :param mutationChance:
+    :return:
+    """
+    if mutationChance == 0:
+        return
+
+    for i, v in enumerate(chromosome):
+        if random() < mutationChance:
+            chromosome[i] = 1-v
+
+
+def mutatePop(population, mutationChance=0.1):
+    """
+    Mutate a population by flipping bits on the chromosomes
+    :param population:
+    :param mutationChance:
+    :return:
+    """
     if mutationChance == 0:
         return
 
     for chromosome in population:
-        for i, v in enumerate(chromosome):
-            if random() < mutationChance:
-                chromosome[i] = 1-v
+        mutate(chromosome, mutationChance)
 
 
-def gaKnapsack(items, capacity, popSize=50, numGenerations=20, mutationChance=0.01):
+def gaKnapsack(items, capacity, popSize=20, numGenerations=20, mutationChance=0.01):
     # Initialise a population and calculate an initial fitness
     population = getRandomPopulation(popSize, numItems)
-    fitnessScores = getFitness(population, items, capacity)
-    print("Initial fitness:", fitnessScores[0][1])
 
     for generation in range(numGenerations):
         # 1) Selection
-        selected, rejected = select(fitnessScores)
+        selected, rejected = select(population, items, capacity)
+        print("Generation {} best fitness: {}".format(generation, selected[0][1]))
 
         # 2) Crossover
+        # keep just the chromosomes
+        selected = [x[0] for x in selected]
         children = crossover(selected)
 
         # 3) Form new population
@@ -122,20 +199,18 @@ def gaKnapsack(items, capacity, popSize=50, numGenerations=20, mutationChance=0.
         # 3.5) population maintenance
         while len(population) < popSize:
             i = randrange(len(rejected))
-            population.append(rejected.pop(i))
+            runt = rejected.pop(i)
+            population.append(runt[0])
 
-        # 3) Mutation with elitism
+        # 4) Mutation with elitism
         # keep the best performing chromosome
         best = population[0]
         # mutate the rest
         rest = population[1:]
-        mutate(rest, mutationChance)
+        mutatePop(rest, mutationChance)
         population = [best] + rest
 
-        # 5) Calculate fitness for our new population
-        fitnessScores = getFitness(population, items, capacity)
-        print("Generation {} fitness: {}".format(generation, fitnessScores[0][1]))
-
+    fitnessScores = getFitness(population, items, capacity)
     bestChromosome = fitnessScores[0][0]
     bestCombination = getPhenotype(bestChromosome, items)
     bestValue = sum([i.value for i in bestCombination])
@@ -146,8 +221,8 @@ def gaKnapsack(items, capacity, popSize=50, numGenerations=20, mutationChance=0.
 if __name__ == "__main__":
     results = {}
     popSizes = [10, 20, 50]
-    mutationChances = [0, 0.01, 0.03, 0.1]
-    numItems = 25
+    mutationChances = [0, 0.01, 0.03]
+    numItems = 50
 
     items = [getRandomItem() for x in range(numItems)]
     capacity = 10 * numItems
